08ee64cf49539223581fd338b8528f75fc38f6e3
Grecia Coronado Garcia <80738908+GreciaCoronado@users.noreply.github.com>
2021-07-01 03:11:20 -0500

    Update app.js

--- codigo/src/app.js
+++ codigo/src/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 80;
 app.set("view engine","ejs");
 
 app.listen(PORT,function(){
-    console.log("Servidor listo, corriendo en puerto 3000");
+    console.log("Servidor listo, corriendo en puerto 80");
 });
 
 app.use(express.static(path.resolve(__dirname +"/../public")));
