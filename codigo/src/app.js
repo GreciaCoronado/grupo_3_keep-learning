47a1a5b27b2efef37fa1f7bb8c475323bd573861
daviders98 <davider98@hotmail.com>
2021-07-01 00:28:50 -0600

    test 2

--- codigo/src/app.js
+++ codigo/src/app.js
@@ -9,7 +9,8 @@ app.listen(PORT,function(){
     console.log("Servidor listo, corriendo en puerto 3000");
 });
 
-app.use(express.static(path.resolve(__dirname,"../public")));
+app.use('/static', express.static(__dirname + '/public'));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false})); 
 
